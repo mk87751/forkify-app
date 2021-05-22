d8e29601a42f18475f244d4afc32920290fdd69f
mk87751 <mk87751@gmail.com>
2021-05-22 13:36:17 +0530

    rendering recipe from API added

---
+++ src/js/controller.js
@@ -0,0 +1,44 @@
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+import * as model from './model';
+import recipeView from './views/recipeView';
+
+const recipeContainer = document.querySelector('.recipe');
+const timeout = function (s) {
+  return new Promise(function (_, reject) {
+    setTimeout(function () {
+      reject(new Error(`Request took too long! Timeout after ${s} second`));
+    }, s * 1000);
+  });
+};
+
+// https://forkify-api.herokuapp.com/v2
+
+///////////////////////////////////////
+const controlRecipes = async function () {
+  try {
+    const id = window.location.hash.slice(1);
+    console.log('id:', id);
+    if (!id) return;
+
+    //rendering recipeSpinner
+    recipeView.renderSpinner();
+
+    //1) loading recipe
+    await model.loadRecipe(id);
+    // const { recipe } = model.state;
+
+    //2) rendering recipe
+    recipeView.render(model.state.recipe);
+  } catch (err) {
+    alert(err);
+  }
+};
+// controlRecipes();
+
+//rendering when load and changing the link
+['hashchange', 'load'].forEach(ev =>
+  window.addEventListener(ev, controlRecipes)
+);
+// window.addEventListener('hashchange', controlRecipes);
+// window.addEventListener('load', controlRecipes);
